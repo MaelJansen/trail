cabaedd2c0d85cd3e8c1decc5bdc12521af0a3ad
juledupont <jules.dupont.1@etu.u-bordeaux.fr>
2023-12-11 16:27:26 +0100

    no more OpenApi

--- back/src/Controller/ApiController.php
+++ back/src/Controller/ApiController.php
@@ -8,10 +8,8 @@ use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
-use FOS\RestBundle\Controller\Annotations\View;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use App\Security\AccessTokenHandler;
@@ -25,28 +23,7 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 #[Route('/api', name: 'app_api')]
 class ApiController extends AbstractController
 {
-    #[OA\Post(
-        summary: "Login user"
-    )]
-    #[OA\Response(
-        response: 200,
-        description: "user token",
-        content: new OA\JsonContent(
-            example:'token : 1ec63c1099c9a5b3fe88745b694055375ddc2ec95a5b63506089cb3e23d76e29'
-        )
-    )]
-    #[OA\Parameter(
-        name: 'mail',
-        description: 'User username',
-        required: true,
-        schema: new OA\Schema(type: 'string')
-    )]
-    #[OA\Parameter(
-        name: 'password',
-        description: 'User password',
-        required: true,
-        schema: new OA\Schema(type: 'string')
-    )]
+    
     #[Route("/login", name: "_login", methods: ["POST"])]
     public function login(#[CurrentUser] ?User $user, AccessTokenHandler $accessTokenHandler): Response
     {
@@ -56,33 +33,7 @@ class ApiController extends AbstractController
         return new JsonResponse(["token" => $accessTokenHandler->createAccessToken($user)]);
     }
 
-     #[OA\Post(
-        summary: "Register user"
-    )]
-    #[OA\Parameter(
-        name: 'email',
-        description: 'Email',
-        required: true,
-        schema: new OA\Schema(type: 'mail')
-    )]
-    #[OA\Parameter(
-        name: 'Firstname',
-        description: 'Firstname',
-        required: true,
-        schema: new OA\Schema(type: 'string')
-    )]
-    #[OA\Parameter(
-        name: 'Lastname',
-        description: 'Lastname',
-        required: true,
-        schema: new OA\Schema(type: 'srting')
-    )]
-    #[OA\Parameter(
-        name: 'password',
-        description: 'User password',
-        required: true,
-        schema: new OA\Schema(type: 'string')
-    )]
+    
     #[Route("/register", name: "_register", methods: ["POST"])]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
