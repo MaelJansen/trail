fd221173c17ebabdb03eb78350f18f4752ba40f6
ctomera <ctomera@etu.u-bordeaux.fr>
2023-12-14 14:55:36 +0100

    factorizing race element

--- back/assets/react/components/Event.jsx
+++ back/assets/react/components/Event.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { Accordion, Container, List, Header, Segment, Grid, Label, Statistic, Icon, Transition } from 'semantic-ui-react';
+import Race from './Race';
+
 
 export default function Event(props) {
     const { events } = props;
@@ -21,7 +23,7 @@ export default function Event(props) {
                         <Accordion styled fluid style={{ marginBottom: '2em' }}>
                             <Accordion.Title
                                 active={activeIndex === index}
-                                index={index}
+                                index={event.id-1}
                                 onClick={handleClick}
                             >
                                 <Header as='h1'><Link to={`/event/${event.id}`}>{event.Name}</Link></Header>
@@ -35,44 +37,13 @@ export default function Event(props) {
                             </Accordion.Title>
                             
                             <Transition visible={activeIndex === index} animation='pulse' duration={250}>
-                            <Accordion.Content active={activeIndex === index}>
                                 <Accordion.Content active={activeIndex === index}>
-                                    <List>
-                                        {Array.isArray(event.Race) && event.Race.map((race, raceIndex) => (
-                                            <Segment>
-                                                <List.Item key={raceIndex}>
-                                                    <Grid columns={2} divided>
-                                                        <Grid.Column>
-                                                            <Grid.Row>
-                                                                <List.Header as='h3'>{race.Name}</List.Header>
-                                                            </Grid.Row>
-                                                            <Grid.Row >
-                                                                <Label.Group>
-                                                                    <Label>2020</Label>
-                                                                    <Label>{race.Address}</Label>
-                                                                </Label.Group>
-                                                            </Grid.Row>
-                                                        </Grid.Column>
-                                                        <Grid.Column verticalAlign='middle' textAlign='center'>
-                                                            <Grid.Row>
-                                                                <Statistic size='mini'>
-                                                                    <Statistic.Value>{race.Distance} km</Statistic.Value>
-                                                                    <Statistic.Label >Distance</Statistic.Label>
-                                                                </Statistic>
-                                                                <Statistic size='mini' label='Dénivelé Positif' value={`${race.PositiveDifference} m`} />
-                                                                <Statistic size='mini' label='Dénivelé Négatif' value={`${race.NegativeDifference} m`} />
-                                                            </Grid.Row>
-                                                        </Grid.Column>
-                                                    </Grid>
-                                                </List.Item>
-                                            </Segment>
-                                        ))}
-                                    </List>
+                                    <Accordion.Content active={activeIndex === index}>
+                                        <Race races={event.Race} />
+                                    </Accordion.Content>    
                                 </Accordion.Content>
-                            </Accordion.Content>
                             </Transition>
                         </Accordion>
-                    
                 </div>
             ))}
         </Container>
