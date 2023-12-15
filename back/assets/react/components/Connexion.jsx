940ac475775b23d95e76eb536c67ab5c0868a8b5
MaelJansen <maeljansen2003@gmail.com>
2023-12-15 11:15:06 +0100

    fix du login

--- back/assets/react/components/Connexion.jsx
+++ back/assets/react/components/Connexion.jsx
@@ -1,52 +1,55 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
-import "semantic-ui-css/semantic.min.css"
-import { Button, Form, Grid, Segment } from "semantic-ui-react"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Form, Grid, Segment } from "semantic-ui-react";
 
 const LoginForm = () => {
   const navigate = useNavigate();
-  
-  const [inputs, setInputs] = useState({email: "", password: ""});
-  const [data, setData] = useState({});
 
-  if(localStorage.getItem("userId")){
+  const [inputs, setInputs] = useState({ email: "", password: "" });
+  const [data, setData] = useState({});
+  const [message, setMessage] = useState(false);
+
+  if (localStorage.getItem("userId")) {
     navigate("/");
   }
-  
+
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}));
-  }
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     getToken();
-  }
+  };
 
-  useEffect(() =>{
+  useEffect(() => {
     if (data.token) {
       localStorage.setItem("token", data.token);
       localStorage.setItem("userId", data.userId);
-      navigate('/');
+      navigate("/");
     } else {
       return;
-    } 
-  }, [data])
+    }
+  }, [data]);
 
   function getToken() {
     let serverQuery = `http://localhost:8000/api/login`;
     axios
-    .post(serverQuery, inputs)
-    .then(response => {
-      setData(response.data);
-    })
-    .catch(error => {
-      setData({
-        "error": error.response.status
+      .post(serverQuery, inputs)
+      .then((response) => {
+        setMessage(false);
+        setData(response.data);
       })
-    })
+      .catch((error) => {
+        setMessage(true);
+        setData({
+          error: error.response.status,
+        });
+      });
   }
 
   return (
@@ -70,17 +73,22 @@ const LoginForm = () => {
               value={inputs.password}
               onChange={handleChange}
               placeholder="Mot de passe"
-              required  
+              required
             />
             <Button color="yellow" fluid size="large" type="submit">
               Login
             </Button>
           </Segment>
         </Form>
+        {message && (
+          <p style={{ color: "red" }}>
+            Mauvais nom d'utilisateur ou mauvais mot de passse
+          </p>
+        )}
         <a href="Register">Pas encore de compte ?</a>
       </Grid.Column>
     </Grid>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '81da845dc0170f9e9414a11b786266c9c502d826',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '81da845dc0170f9e9414a11b786266c9c502d826',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/password-hasher' => array(
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => 'd2da68c2f7a240bd6edf7e96fdc7aca5e7beea66',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/password-hasher',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
